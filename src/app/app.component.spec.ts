7969b231cacd7db8dbfc9219770250169818669c
jaegerph <philipp.jaeger@th-nuernberg.de>
2023-03-31 16:43:20 +0200

    Renamed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'github-deploy'`, () => {
+  it(`should have as title 'agile-swe-reifegradmodell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('github-deploy');
+    expect(app.title).toEqual('agile-swe-reifegradmodell');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('github-deploy app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('agile-swe-reifegradmodell app is running!');
   });
 });
