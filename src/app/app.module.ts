46c68bca17ac2b8564b6a8bf331aa871aa0aa87c
jaegerph <philipp.jaeger@th-nuernberg.de>
2023-04-07 11:58:20 +0200

    Added routes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { SurveyComponent } from './survey/survey.component';
+import { ResultsComponent } from './results/results.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionComponent,
+    SurveyComponent,
+    ResultsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    ButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
