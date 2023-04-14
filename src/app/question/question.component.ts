c8fdd6a623bc20823113de1b90092ca03505eb9f
CodeTriager <testh4321@gmail.com>
2023-04-14 12:16:13 +0200

    added more logic

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import * as questions from '../../assets/questions.json';
+import { SingleQuestion } from '../survey/survey.component';
 
 @Component({
   selector: 'app-question',
@@ -7,23 +8,41 @@ import * as questions from '../../assets/questions.json';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+
+  @Input() question: SingleQuestion = {} as SingleQuestion;
+  @Output() nextButton = new EventEmitter<boolean>();
+  @Output() previousButton = new EventEmitter<boolean>();
+
   categories: any[] = [
-    
+
     { name: 'Es findet kaum Austausch im Entwicklerteam statt (jeder Arbeitet für sich an seinen Aufgaben)', key: '0' },
-    
+
     { name: 'Das gesamte Entwicklerteam hat die Möglichkeit sich untereinander abzustimmen', key: '1' },
-    
+
     { name: 'Es gibt einen festgelegten Rahmen zur regelmäßigen Besprechung (bsp. wöchentlich)', key: '2' },
-    
+
     { name: 'Es finden jeden Tag zu festgelegter Uhrzeit im kompletten Team Dailys statt. ', key: '3' },
-    
+
     { name: 'Es finden jeden Tag zu festgelegter Uhrzeit im  Team Dailys statt. Es wird auf eine kurze Dauer und Effektivität geachtet.', key: '4' }
-    
+
   ];
-  
+
   selectedCategory: any = null;
 
+  constructor() {
+  }
+
   ngOnInit(): void {
-    console.log(questions);
+    console.log('question', this.question);
+  }
+
+  public nextButtonClicked() {
+    console.log('next button clicekd');
+    this.nextButton.emit(true);
+  }
+
+  public previousButtonClicked() {
+    this.previousButton.emit(true);
   }
 }
+
