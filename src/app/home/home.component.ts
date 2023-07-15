2d9ac271a06d83f35fca950d4617ea8704287435
Jan Distler <j.distler1702@outlook.de>
2023-07-15 14:01:32 +0200

    Added experts (deleted carousel)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,42 +21,39 @@ export class HomeComponent {
 
   ngOnInit() {
     this.experts = [
-      new Expert("Expert1", "Description1", "Link1"),
-      new Expert("Expert2", "Description2", "Link2"),
-      new Expert("Expert3", "Description3", "Link3"),
-      new Expert("Expert4", "Description4", "Link4"),
-      new Expert("Expert5", "Description5", "Link5"),
-      new Expert("Expert6", "Description6", "Link6"),
-    ];
-    this.responsiveOptions = [
-      {
-        breakpoint: '1400px',
-        numVisible: 3,
-        numScroll: 1
-      },
-      {
-        breakpoint: '1000px',
-        numVisible: 2,
-        numScroll: 1
-      },
-      {
-        breakpoint: '750px',
-        numVisible: 1,
-        numScroll: 1
-      }
+      new Expert(
+        "Javier Gutierrez",
+        "Agile Coach bei BRANDAD Development GmbH",
+        "../../assets/images/BRANDAD_Development.svg",
+        "https://brandad.dev/"
+        ),
+      new Expert(
+        "Kevin Edelmann",
+        "Scrum Master bei CodeCamp:N",
+        "../../assets/images/codecamp_n_logo.svg",
+        "https://www.codecamp-n.com/"
+      ),
+      new Expert(
+        "Siemens",
+        "Scrum Master bei Siemens",
+        "../../assets/images/siemens-logo-default.svg",
+        "https://www.siemens.com/de/de.html"
+      )
     ];
   }
 }
 
 class Expert {
-  title: string = "";
+  name: string = "";
   description: string = "";
-  link: string = "";
+  company_image: string = "";
+  company_link: string = "";
 
-  constructor(title: string, description: string, link: string) {
-    this.title = title;
+  constructor(name: string, description: string, company_image: string, company_link: string) {
+    this.name = name;
     this.description = description;
-    this.link = link;
+    this.company_image = company_image;
+    this.company_link = company_link;
   }
 }
 
